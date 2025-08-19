690ec6046883e88c25e24d44a95698f66ef25d6f
kkeman <kkeman11@gmail.com>
2025-08-19 17:01:33 +0900

    mediation only

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.9
+import PackageDescription
+let package = Package(
+    name: "AdMixerMediation",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "AdMixerMediation", targets: ["AdMixerMediation"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AdMixerMediation",
+            url: "https://github.com/Nasmedia-Tech/iOS-SSP-SPM/raw/main/AdMixerMediation2.0.5.xcframework.zip",
+            checksum: "865a4a6c1e9a89c805e2089956b43d45ed7f533c1308dc484dc60870b096cb76"
+        ),
+    ]
+)
