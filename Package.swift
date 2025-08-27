d746f523b89f4bbdd05a2f9339416f30ee61f275
kkeman <kkeman11@gmail.com>
2025-08-27 16:11:32 +0900

    v2.0.8
    admixer 매체 직접 연동

--- Package.swift
+++ Package.swift
@@ -8,26 +8,11 @@ let package = Package(
             name: "AdMixerMediation",
             targets: ["AdMixerMediation"]),
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/Nasmedia-Tech/iOS-SSP-SPM.git",
-            exact: "1.0.8"
-        )
-    ],
     targets: [
         .binaryTarget(
             name: "AdMixerMediation",
-            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediation2.0.7.xcframework.zip",
-            checksum: "54a1e40744ab8823e1d2a185042dfbda70d6151b1ae9d8a4f524ca793d4e133b"
-        ),
-        .target(
-            name: "AdMixerMediationWrapper",
-            dependencies: [
-                "AdMixerMediation",
-                .product(
-                    name: "AdMixer",
-                    package: "ios-ssp-spm"),
-            ]
+            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediation2.0.8.xcframework.zip",
+            checksum: "dade8219c9e5c41f418fc7cf797a9fb1f33446677de501fce44d1a0aa981544b"
         ),
     ]
 )
