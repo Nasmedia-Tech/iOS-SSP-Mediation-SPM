a1e80baefa42d7c1faca7985e9f8693ad40d4fd7
seony42 <seony42@nasmedia.co.kr>
2026-01-16 11:19:41 +0900

    chore: v2.1.5

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdMixerMediation",
-            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediation2.1.4.xcframework.zip",
-            checksum: "8e1f72658b684dec74e4fe69f82366c20f98625e892ab2f754ee64ecba7924a5"
+            url: "https://github.com/Nasmedia-Tech/iOS-AdMixerDownload/raw/main/AdMixerMediation2.1.5.xcframework.zip",
+            checksum: "e30c7e380ccf110039cb5cee4a9f367b803ac83d6d9b29546bde8af461694032"
         ),
     ]
 )
