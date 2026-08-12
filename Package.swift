853e659466dd00e5e7755c47f377cbd8e077aaf3
seony42 <seony42@nasmedia.co.kr>
2026-08-12 17:09:16 +0900

    release: AdMixerMediation 2.4.4

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdMixerMediation",
-            url: "https://github.com/Nasmedia-Tech/iOS-SSP-Mediation-SPM/releases/download/2.4.3/AdMixerMediation2.4.3.xcframework.zip",
-            checksum: "d4e6ada7bdbafc9619027d6a0d4690e136dc54453a9c035926373d08c1f7c8e8"
+            url: "https://github.com/Nasmedia-Tech/iOS-SSP-Mediation-SPM/releases/download/2.4.4/AdMixerMediation2.4.4.xcframework.zip",
+            checksum: "aec6235e475f3e89cf1bbd7889d91886e0e5155b8320cac509a919fdd165aa81"
         ),
     ]
 )
